c7583e68a7c47b6ccdf4c8ffcb29ccdf01a0d4a4
Bernardo Carvalho <nal-carvalho@hotmail.com>
2025-06-20 17:29:02 -0300

    feat: Creating controller and view files

--- Gemfile
+++ Gemfile
@@ -47,6 +47,9 @@ gem "bootsnap", require: false
 
 gem 'devise'
 
+gem 'view_component'
+
+
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :accounts
+  resources :categories
+  resources :transactions
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
