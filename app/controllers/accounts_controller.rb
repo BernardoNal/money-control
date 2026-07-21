87bf8c77366c69198ae9cace2bb9a5050db39dff
Bernardo Carvalho <nal-carvalho@hotmail.com>
2026-07-21 19:13:21 -0300

    fix: restore user ownership for accounts and categories

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,7 +1,8 @@
 class AccountsController < ApplicationController
   before_action :set_account, only: %i[show edit update destroy]
+
   def index
-    @accounts = Account.all
+    @accounts = current_user_accounts
   end
 
   def show
@@ -13,26 +14,23 @@ class AccountsController < ApplicationController
   end
 
   def create
-    # @user = current_user
-     @user = User.first
     @account = Account.new(account_params)
-    @account.user = @user
+    @account.user = current_user
+
     if @account.save
-      redirect_to account_path(@account)
-      flash[:alert] = "Conta criada com sucesso."
+      redirect_to account_path(@account), notice: "Conta criada com sucesso."
     else
-      render :new, status: :unprocessable_entity
+      render :form, status: :unprocessable_entity
     end
   end
 
   def edit
-     render :form
+    render :form
   end
 
   def update
     if @account.update(account_params)
-      redirect_to account_path(@account)
-      flash[:alert] = "Conta alterada com sucesso."
+      redirect_to account_path(@account), notice: "Conta alterada com sucesso."
     else
       render :form, status: :unprocessable_entity
     end
@@ -40,19 +38,22 @@ class AccountsController < ApplicationController
 
   def destroy
     @account.destroy
-    flash[:alert] = "Conta excluída com sucesso."
 
-    redirect_to accounts_path()
+    redirect_to accounts_path, notice: "Conta excluida com sucesso."
   end
 
   private
 
   # Permits account parameters
   def account_params
-    params.require(:account).permit( :name, :card_type, :bank, :initial_balance, :limit, :last_digits,:due_day)
+    params.require(:account).permit(:name, :card_type, :bank, :initial_balance, :limit, :last_digits, :due_day)
   end
 
   def set_account
-    @account = Account.find(params[:id])
+    @account = current_user_accounts.find(params[:id])
+  end
+
+  def current_user_accounts
+    Account.where(user: current_user)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :accounts, dependent: :destroy
   has_many :investment_portfolios, class_name: "Investments::Portfolio", dependent: :destroy
 
   validates :name, presence: true
